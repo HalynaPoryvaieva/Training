bf88ef1ef49e1ec0d390169bdefe6f8da20ea6ba
HalynaPoryvaieva <lastiklastik407@gmail.com>
2024-03-21 14:26:49 +0100

    Task2

--- src/Task1.java
+++ src/Task1.java
@@ -1,23 +1,22 @@
+//Let's write a program that will read age from the keyboard. If age is from 20 to 60 (inclusive),
+// then you don’t need to output anything, otherwise, output the phrase “you don’t have to work.” This can (and should!) be done with
+// using only one if statement without else.
+//
+//Hint: use the logical operator "||" (or).
+//
+//Requirements:
+//• The program must read one number from the keyboard.
+//• If the age is less than 20 or more than 60, then you need to display the phrase “you don’t have to work.”
+//• If the age is in the range from 20 to 60 (inclusive), then nothing needs to be output.
+//• The program must use one if statement without else and the logical operator ||.
 
-//Напишем программу, которая будет считывать с клавиатуры возраст. Если возраст от 20 до 60 (включительно),
-// то выводить ничего не нужно, иначе - вывести фразу "можно не работать". Сделать это можно (и нужно!) с
-// помощью только одного оператора if без else.
-//
-//Подсказка: используй логический оператор "||" (или).
-//
-//Требования:
-//•	Программа должна считывать одно число c клавиатуры.
-//•	Если возраст меньше 20 или больше 60, то нужно вывести фразу "можно не работать".
-//•	Если возраст в диапазоне от 20 до 60 (включительно), то выводить ничего не нужно.
-//•	В программе должен использоваться один оператор if без else и логический оператор ||.
+import java.util.Scanner;
 
 /**
  * AIT-TR,Java Basic,Task1
  * @version 21-Mar-24
  * @author Halyna Poryvaieva
  */
-import java.util.Scanner;
-
 public class Task1 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
---
+++ src/Task2.java
@@ -0,0 +1,52 @@
+/*
+Enter two integers from the keyboard, which will be the coordinates of the point (the first number read is the "x" coordinate,
+// and the second is the "y" coordinate). It is known that the point does not lie on the coordinate axes OX and OY.
+//Display the number of the coordinate quarter in which the given point is located.
+//
+//Clue:
+//Belonging of a point with coordinates (x, y) to one of the quarters is determined as follows:
+//
+//for the first quarter x>0 and y>0;
+//for the second quarter x<0 and y>0;
+//for the third quarter x<0 and y<0;
+//for the fourth quarter x>0 and y<0.
+//Example for numbers 4 6:
+//1
+//Example for numbers -6 -6:
+//3
+//Requirements:
+//• The program must read two numbers from the keyboard.
+//• The program must use the System.out.println() or System.out.print() command.
+//• If the point is in the first coordinate quarter, print "1".
+//• If the point is in the second coordinate quadrant, print "2".
+//• If the point is in the third coordinate quadrant, print "3".
+//• If the point is in the fourth coordinate quadrant, print "4".
+*/
+
+import java.util.Scanner;
+
+/**
+ * AIT-TR,Java Basic,Task2
+ * @version 21-Mar-24
+ * @author Halyna Poryvaieva
+ */
+public class Task2 {
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter two numbers:");
+
+        int x= scanner.nextInt();
+        int y= scanner.nextInt();
+
+        if (x>0&y>0){
+            System.out.println("1");
+        }else if (x<0&y>0){
+            System.out.println("2");
+        }else if (x<0&y<0){
+            System.out.println("3");
+        }else if (x>0&y<0){
+            System.out.println("4");
+        }
+    }
+}
